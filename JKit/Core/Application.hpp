82c0c78ff3c58b34b6befa57e083c2cc3addcae1
lxndr <lxndr87i@gmail.com>
2014-12-26 10:01:06 +0300

    Add basic J::Application

---
+++ JKit/Core/Application.hpp
@@ -0,0 +1,50 @@
+#ifndef __JKIT__CORE__APPLICATION_HPP__
+#define __JKIT__CORE__APPLICATION_HPP__
+
+#include <JKit/Core/String.hpp>
+
+
+namespace J {
+
+
+class Application
+{
+public:
+	Application() {
+	}
+
+
+	virtual ~Application() {
+		
+	}
+
+
+	int run(int argc, char** argv) {
+		startup();
+	}
+
+
+	virtual void startup() {
+	}
+
+
+	virtual void commandLine(const StringList& args) {
+	}
+};
+
+
+}
+
+
+#ifdef JKIT_PLATFORM_LINUX
+
+#define JKIT_MAIN(app_class)			\
+	int main(int argc, char** argv) {	\
+		app_class app;					\
+		return app.run(argc, argv);		\
+	}
+
+#endif
+
+
+#endif
